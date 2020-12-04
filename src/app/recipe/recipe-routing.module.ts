35cc425ad36e4185f886b39849353bfb9d3e0896
Viktor Kombov <viktorkombov@gmail.com>
2020-12-05 00:45:29 +0200

    new commit

--- src/app/recipe/recipe-routing.module.ts
+++ src/app/recipe/recipe-routing.module.ts
@@ -1,15 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeNewComponent } from './recipe-new/recipe-new.component';
 
 const routes: Routes = [
     {
         path: 'recipe/details',
         component: RecipeDetailsComponent
     },
-    // {
-    //   path: '**',
-    //   component: NotFoundComponent
-    // }
+    {
+      path: 'recipe/new',
+      component: RecipeNewComponent
+    }
 ];
 
 export const RecipeRoutingModule = RouterModule.forChild(routes);
--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { RecipeRoutingModule } from './recipe-routing.module';
+import { RecipeNewComponent } from './recipe-new/recipe-new.component';
 
 
 
 @NgModule({
-  declarations: [RecipeDetailsComponent],
+  declarations: [RecipeDetailsComponent, RecipeNewComponent],
   imports: [
     CommonModule,
     RecipeRoutingModule
