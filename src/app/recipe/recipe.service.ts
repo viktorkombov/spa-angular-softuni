5454bf7cffe1145df4535743bdb6ad4a401a1222
Viktor Kombov <viktorkombov@gmail.com>
2020-12-12 16:00:12 +0200

    n c

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -10,29 +10,29 @@ export class RecipeService {
   constructor(private http: HttpClient) { }
 
   loadRecipeList(): Observable<IRecipe[]> {
-    return this.http.get<IRecipe[]>(`http://localhost:3000/api/recipes`);
+    return this.http.get<IRecipe[]>(`https://your-recipes-expressjs-server.herokuapp.com/api/recipes`);
   }
 
   loadRecipe(id: string): Observable<IRecipe<IUser>> {
-    return this.http.get<IRecipe>(`http://localhost:3000/api/recipes/${id}`).pipe(
+    return this.http.get<IRecipe>(`https://your-recipes-expressjs-server.herokuapp.com/api/recipes/${id}`).pipe(
       tap(((recipe: IRecipe) => this.recipe = recipe)),
       catchError(() => { this.recipe = null; return of(null); })
     )
   }
 
   saveRecipe(data: any): Observable<IRecipe<IUser>> {
-    return this.http.post<IRecipe<any>>(`http://localhost:3000/api/recipes`, data, {withCredentials: true});
+    return this.http.post<IRecipe<any>>(`https://your-recipes-expressjs-server.herokuapp.com/api/recipes`, data, {withCredentials: true});
   }
 
   likeRecipe(id: string): Observable<IRecipe> {
-    return this.http.put<IRecipe>(`http://localhost:3000/api/recipes/${id}`, {}, {withCredentials: true});
+    return this.http.put<IRecipe>(`https://your-recipes-expressjs-server.herokuapp.com/api/recipes/${id}`, {}, {withCredentials: true});
   }
 
   deleteRecipe(id: string): Observable<any> {
-    return this.http.post<IRecipe>(`http://localhost:3000/api/recipes/delete/${id}`, {}, {withCredentials: true});
+    return this.http.post<IRecipe>(`https://your-recipes-expressjs-server.herokuapp.com/api/recipes/delete/${id}`, {}, {withCredentials: true});
   }  
 
   editRecipe(id: string, data): Observable<IRecipe> {
-    return this.http.post<IRecipe>(`http://localhost:3000/api/recipes/edit/${id}`, data, {withCredentials: true});
+    return this.http.post<IRecipe>(`https://your-recipes-expressjs-server.herokuapp.com/api/recipes/edit/${id}`, data, {withCredentials: true});
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/',
-  apiUrl: 'http://localhost:3000/api/'
+  baseUrl: 'https://your-recipes-expressjs-server.herokuapp.com',
+  apiUrl: 'https://your-recipes-expressjs-server.herokuapp.com/api/'
 };
 
 /*
